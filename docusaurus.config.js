b6bdb8ababa613d008aa03b3595ca3453b970123
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2022-01-07 13:03:27 -0800

    fix baseUrl with trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Build local-first software',
   tagline: 'Automerge is a library of data structures for building collaborative applications that work offline.',
   url: 'https://automerge.github.io',
-  baseUrl: '/docs',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
